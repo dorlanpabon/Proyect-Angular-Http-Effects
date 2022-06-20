e38bfb2eb8f6c71ccc7e0153e36ca7f2a7f8469e
dorlanpabon <camilopa1998@hotmail.com>
2022-06-20 11:07:00 -0500

    Creando estructura de carpetas y modulo usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { ListaComponent } from './usuarios/lista/lista.component';
+import { UsuarioComponent } from './usuarios/usuario/usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuariosComponent,
+    ListaComponent,
+    UsuarioComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
